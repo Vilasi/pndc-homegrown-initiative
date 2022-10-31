86d25bd2118c6705c3407577ff85214f91bf8b03
Vilasi <VilasiCoding@gmail.com>
2022-10-31 04:07:29 -0400

    Staggered CSS/JS El loading animation first init

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-const a = document.querySelector('#tour-btn');
-console.log(a);
-
-// const c = console.log.bind(document);
 const c = console.log;
+const cd = console.dir;
 
-c(123);
+const workSection = document.querySelector('.diagonal-box-1');
 
-c(a);
+const scrollInElems = document.querySelectorAll('.scrollIn');
+// c(scrollInElems);
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    // console.log(entry);
+    if (entry.isIntersecting) {
+      c(entry.target);
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+scrollInElems.forEach((el) => observer.observe(el));
+
+// const tourBtn = document.querySelector('#tour-btn');
+// tourBtn.addEventListener('click', (e) => {
+//   const bounds = workSection.getBoundingClientRect();
+//   //   scrollIntoView(true, { behavior: 'smooth', block: 'start' });
+//   c(bounds.top);
+//   window.scrollTo(0, bounds.top);
+//   //   window.scrollY(bounds.top);
+// });
+
+// c(workSection.getBoundingClientRect());
