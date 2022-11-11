0478eb28190046bda31c566c9cd989d6cd4f2c2c
Vilasi <VilasiCoding@gmail.com>
2022-11-10 20:13:02 -0500

    Added JS for navbar hiding on scroll down

--- app.js
+++ app.js
@@ -19,3 +19,41 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 scrollInElems.forEach((el) => observer.observe(el));
+
+//get nav
+const nav = document.querySelector('.navbar');
+const goMercerCounty = document.querySelector('#goMercerCounty');
+const mobileNavButton = document.querySelector('#mobile-nav-button');
+// nav.style.background = 'rgba(0,0,0,0.5)';
+// nav.classList.toggle('transparent-background');
+// nav.classList.remove('show');
+// goMercerCounty.classList.add('solid-background');
+// mobileNavButton.classList.add('solid-background');
+
+// console.log(screen.width);
+
+//hide nav until the user scrolls down
+let prevScrollpos = window.scrollY;
+window.onscroll = function () {
+  let currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    nav.style.top = '0';
+  } else {
+    nav.style.top = '-100px';
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+window.addEventListener('scroll', function () {
+  console.log(window.scrollY);
+});
+
+//In portait orientation, the nav bar is transparent until the user scrolls down
+// function isPortrait() {
+//   return window.innerHeight > window.innerWidth;
+// }
+// if (isPortrait()) {
+//   nav.classList.toggle('transparent-background');
+// } else {
+//   nav.classList.remove('transparent-background');
+// }
