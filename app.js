dd50a2478ca23fd246ebb23e58b14d602684d354
Vilasi <VilasiCoding@gmail.com>
2022-11-15 03:59:30 -0500

    Init work.html, styles.css, JS Button hrefs script added in app.js

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const c = console.log;
 const cd = console.dir;
+const BUTTONS = document.querySelectorAll('button');
 
 // const workSection = document.querySelector('.diagonal-box-1');
 
 // c(scrollInElems);
 
+// The ScrollIn Animation Effect script
 const scrollInElems = document.querySelectorAll('.scrollIn');
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     // console.log(entry);
     if (entry.isIntersecting) {
-      c(entry.target);
+      // c(entry.target);
       entry.target.classList.add('show');
     } else {
       // entry.target.classList.remove('show');
@@ -36,6 +38,30 @@ window.onscroll = function () {
   prevScrollpos = currentScrollPos;
 };
 
-window.addEventListener('scroll', function () {
-  console.log(window.scrollY);
+// window.addEventListener('scroll', function () {
+//   console.log(window.scrollY);
+// });
+
+console.log(BUTTONS);
+BUTTONS.forEach((btn) => {
+  btn.addEventListener('click', buttonDirectory);
 });
+
+// List of pages and other links to attach to buttons
+const pages = {
+  work: './pages/work.html',
+};
+
+// Changes window.location.href on button click to the page specified in the pages object
+function buttonDirectory(e) {
+  console.log('Clicked!!');
+  console.log(e.target.id);
+
+  switch (e.target.id) {
+    case 'work-button':
+      window.location.href = pages.work;
+      break;
+    default:
+      break;
+  }
+}
