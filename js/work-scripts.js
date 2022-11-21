dad2adea918c2987937e2861551403d9c9b9a547
Vilasi <VilasiCoding@gmail.com>
2022-11-20 23:05:22 -0500

    Added date tracking to numbers. Refactored class Iterator. Beginning Stylings

--- js/work-scripts.js
+++ js/work-scripts.js
@@ -1,20 +1,8 @@
-const openJobs = document.querySelector('#open-jobs');
-const two = document.querySelector('#two');
-const three = document.querySelector('#three');
+// const openJobs = document.querySelector('#open-jobs');
+// const two = document.querySelector('#two');
+// const three = document.querySelector('#three');
 
 let iterator = 0;
-
-// const intervalTest = setInterval(() => {
-//   //   printNums(iterator);
-//   openJobs.textContent = iterator;
-//   two.textContent = iterator;
-//   three.textContent = iterator;
-
-//   iterator++;
-//   if (iterator === 7000) {
-//     clearInterval(intervalTest);
-//   }
-// }, 0.01);
 
 class Iterator {
   constructor(num, id, time) {
@@ -23,52 +11,105 @@ class Iterator {
     this.time = time;
     this.iterator = 0;
     this.interval = setInterval(() => {
-      console.log('I am the interval');
-      // console.log(this.iterator);
-      // this.iterator++;
-      // console.log(this.num);
-      // console.log(this.iterator);
       if (this.iterator >= this.num) {
         this.clear();
-        this.writeToDom();
+        this.writeToDom(this.iterator);
       } else if (this.num - this.iterator >= 1000) {
         this.iterator += 500;
-        this.writeToDom();
+        this.writeToDom(this.iterator);
       } else if (this.num - this.iterator > 100) {
         this.iterator += 100;
-        this.writeToDom();
+        this.writeToDom(this.iterator);
       } else {
         this.iterator++;
-        this.writeToDom();
+        this.writeToDom(this.iterator);
       }
     }, this.time);
   }
 
   clear = () => {
-    console.log('Interval clear was successful');
     clearInterval(this.interval);
   };
 
-  writeToDom = () => {
-    this.el.textContent = `${this.iterator}`;
+  writeToDom = (num) => {
+    let stringFromNum = num.toString();
+    let reversedArray = Array.from(stringFromNum).reverse();
+    let arrayFromString = [];
+    let iterator = 0;
+    for (let i = 0; i < reversedArray.length; i++) {
+      if (iterator === 2) {
+        arrayFromString.push(reversedArray[i]);
+        arrayFromString.push(',');
+        iterator = 0;
+      } else {
+        arrayFromString.push(reversedArray[i]);
+        iterator++;
+      }
+    }
+    let stringToPrint = arrayFromString.reverse().join('');
+    this.el.textContent = `${stringToPrint}`;
   };
 }
 
+// Make new instance of Iterator class on Intersection - making neat counting/iterating DOM effect
 let target = document.querySelector('#countingValues');
-console.log(target);
-
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry);
+    // Add classList to increase font-size/width
+    const childrenCollection = entry.target.children;
+    for (el of childrenCollection) {
+      console.log(el.children[0].classList);
+      el.children[0].classList.add('numericals');
+    }
     if (entry.isIntersecting) {
-      const salary = new Iterator(45000, '#one', 0.01);
-      const schools = new Iterator(9, '#two', 250);
-      const jobOpenings = new Iterator(7829, '#three', 35);
-      // setTimeout(() => {
-      // }, 1000);
+      setTimeout(() => {
+        const salary = new Iterator(
+          pickNumberRange(56000, 59000, 58500),
+          '#one',
+          0.01
+        );
+        const schools = new Iterator(9, '#two', 250);
+        const jobOpenings = new Iterator(
+          pickNumberRange(7600, 8050, 7829),
+          '#three',
+          25
+        );
+      }, 250);
+    } else {
+      const salary = new Iterator('', '#one', 0);
+      const schools = new Iterator('', '#two', 0);
+      const jobOpenings = new Iterator('', '#three', 0);
     }
   });
-  console.log(entry);
 });
-
 observer.observe(target);
+
+// generate a random number between 7500 and 8500
+
+// function that generates a number between its first parameter and its second parameter
+function randomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// Check if another day has passed
+function checkDay() {
+  let today = new Date();
+  let day = today.getDate();
+  let month = today.getMonth();
+  let year = today.getFullYear();
+  let hour = today.getHours();
+  let minute = today.getMinutes();
+  let second = today.getSeconds();
+  let millisecond = today.getMilliseconds();
+
+  return day + month + year;
+}
+
+function pickNumberRange(min, max, hardcodedValue) {
+  if (2052 !== checkDay()) {
+    console.log(true);
+    return hardcodedValue;
+  } else {
+    return randomNum(min, max);
+  }
+}
