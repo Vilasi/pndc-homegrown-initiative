ced8338044e1108a211969bafabf24a1b55fae68
Vilasi <VilasiCoding@gmail.com>
2022-12-05 12:52:08 -0500

    Added initial test script for Cost of Living calculator on Live page

---
+++ js/live-scripts.js
@@ -0,0 +1,23 @@
+// https://www.bestplaces.net/cost-of-living/sharon-pa/hermitage-pa/30000
+// const c = console.log;
+
+// const form = document.querySelector('form');
+
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+
+//   const children = e.target.children;
+//   const valueArray = [];
+//   for (let child of children) {
+//     if (child.classList.contains('input')) {
+//       console.log(child.value);
+//       valueArray.push(child.value);
+//     }
+//   }
+//   console.log(valueArray);
+
+//   window.location.href = `https://www.bestplaces.net/cost-of-living/${valueArray[0]}/${valueArray[1]}/${valueArray[2]}`;
+// });
+
+//////////////////// KEEP THE ABOVE
+//////////////////// THIS IS THE BASIS FOR THE COST OF LIVING CALCULATOR COMMANDEERING SCRIPT
